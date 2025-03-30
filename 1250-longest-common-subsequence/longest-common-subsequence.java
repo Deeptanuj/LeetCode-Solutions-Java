aecc9cfdf4531ab0c0612becfd3da224a19f62c3
Deeptanuj Gupta <93655452+Deeptanuj@users.noreply.github.com>
2025-03-30 21:46:24 +0530

    Time: 19 ms (87.40%) | Memory: 50.5 MB (86.79%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,20 +3,19 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         int dp[][] = new int[n + 1][m + 1];
+        for(int i = 0; i < m + 1; i++){
+            dp[0][i] = 0;
+        }
         for(int i = 0; i < n + 1; i++){
-            for(int j = 0; j < m + 1; j++){
-                dp[i][j] = -1;
+            dp[i][0] = 0;
+        }
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < m + 1; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
-        return lcs(text1, text2, n, m, dp);
-    }
-    private int lcs(String x, String y, int n, int m, int dp[][]){
-        if(n == 0 || m == 0) return 0;
 
-        if(dp[n][m] != -1) return dp[n][m];
-
-        if(x.charAt(n-1) == y.charAt(m - 1)) return dp[n][m] = 1 + lcs(x, y, n-1, m-1, dp);
-
-        return dp[n][m] = Math.max(lcs(x, y, n, m-1, dp), lcs(x, y, n-1, m, dp));
+        return dp[n][m];
     }
 }
