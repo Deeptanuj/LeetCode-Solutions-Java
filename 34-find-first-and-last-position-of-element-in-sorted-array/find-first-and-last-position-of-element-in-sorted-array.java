0fc94010aaebbff50dd638ed18dfdf74734159b7
Deeptanuj Gupta <93655452+Deeptanuj@users.noreply.github.com>
2025-04-06 13:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (47.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int ans[] = new int[2];
-        ans[0] = firstPosi(nums, target);
-        ans[1] = lastPosi(nums, target);
+        ans[0] = leftbound(nums, target);
+        ans[1] = rightbound(nums, target);
         return ans;
     }
-    private int firstPosi(int arr[], int n){
-        int l = 0;
-        int r = arr.length - 1;
-        int ans = -1;
-        while(l <= r){
-            int m = l + (r -l) / 2;
-
-            if(arr[m] == n){
-                ans = m;
-                r = m - 1;
-            } else if(arr[m] < n) l = m + 1;
-            else r = m - 1;
-        }
-        return ans;
-    }
-    private int lastPosi(int arr[], int n){
+    private int leftbound(int arr[], int x){
         int l = 0;
         int r = arr.length - 1;
         int ans = -1;
         while(l <= r){
             int m = l + (r - l) / 2;
 
-            if(arr[m] == n){
+            if(arr[m] == x){
+                ans = m;
+                r = m - 1;
+            }
+            else if(arr[m] < x) l = m + 1;
+            else r = m - 1;
+        }
+
+        return ans;
+    }
+    private int rightbound(int arr[], int x){
+        int l = 0;
+        int r = arr.length - 1;
+        int ans = -1;
+        while(l <= r){
+            int m = l + (r - l) / 2;
+
+            if(arr[m] == x){
                 ans = m;
                 l = m + 1;
-            } else if(arr[m] < n) l = m + 1;
+            }
+            else if(arr[m] < x) l = m + 1;
             else r = m - 1;
         }
         return ans;
