4e528bc6c8d79daa8134d45a6fd2fc46f4621d1e
Deeptanuj Gupta <93655452+Deeptanuj@users.noreply.github.com>
2025-06-08 02:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (48.07%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int ans[] = new int[2];
-        ans[0] = leftbound(nums, target);
-        ans[1] = rightbound(nums, target);
+        ans[0] = firstOcc(nums, target);
+        ans[1] = lastOcc(nums, target);
         return ans;
     }
-    private int leftbound(int arr[], int x){
+    private static int firstOcc(int arr[], int target){
         int l = 0;
         int r = arr.length - 1;
         int ans = -1;
         while(l <= r){
             int m = l + (r - l) / 2;
-
-            if(arr[m] == x){
+            if(arr[m] == target){
                 ans = m;
                 r = m - 1;
-            }
-            else if(arr[m] < x) l = m + 1;
+            } else if(arr[m] < target) l = m + 1;
             else r = m - 1;
         }
-
         return ans;
     }
-    private int rightbound(int arr[], int x){
+    private static int lastOcc(int arr[], int target){
         int l = 0;
         int r = arr.length - 1;
         int ans = -1;
         while(l <= r){
             int m = l + (r - l) / 2;
-
-            if(arr[m] == x){
+            if(arr[m] == target){
                 ans = m;
                 l = m + 1;
-            }
-            else if(arr[m] < x) l = m + 1;
+            } else if(arr[m] < target) l = m + 1;
             else r = m - 1;
         }
         return ans;
