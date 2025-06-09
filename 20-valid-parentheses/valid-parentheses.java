c79de3c3edbf0dd5f948f5f7cf567f4ff8c0e662
Deeptanuj Gupta <93655452+Deeptanuj@users.noreply.github.com>
2025-06-09 14:33:55 +0530

    Time: 2 ms (97.23%) | Memory: 41.9 MB (47.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,8 +5,8 @@ class Solution {
             if(bracket == '(') stack.push(')');
             else if(bracket == '{') stack.push('}');
             else if(bracket == '[') stack.push(']');
-            else if(stack.empty() || stack.pop() != bracket) return false;
+            else if(stack.isEmpty() || stack.pop() != bracket) return false;
         }
-        return stack.empty();
+        return stack.isEmpty();
     }
 }
