8c118a9902620690a6165b2396f2d6c93dc22e96
Deeptanuj Gupta <93655452+Deeptanuj@users.noreply.github.com>
2025-06-30 01:07:47 +0530

    Time: 23 ms (24.30%) | Memory: 52.1 MB (15.49%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -6,9 +6,7 @@ class Solution {
         map.put(0, 1);
         for(int i = 0; i < nums.length; i++){
             sum += nums[i];
-            if(map.containsKey(sum - goal)){
-                count += map.get(sum - goal);
-            }
+            if(map.containsKey(sum - goal)) count += map.get(sum - goal);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
